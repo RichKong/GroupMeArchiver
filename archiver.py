4971b97544494843e2a319e8355dee5a7c615830
abrassel <abrassel@users.noreply.github.com>
2019-04-24 21:30:35 -0400

    Created the archiving framework

---
+++ archiver.py
@@ -0,0 +1,10 @@
+class Archiver:
+	def __init__(token):
+		self.token = token
+		
+	def group_list():
+		pass #return list of strings of group names
+		
+	def archive_group(groupNo):
+		pass #create a folder filled with the contents of the group messages
+		
