0fc36649cda7f8247fe49432e44d57a70aa9c506
abrassel <abrassel@users.noreply.github.com>
2019-04-24 21:45:44 -0400

    Update archiver.py

--- archiver.py
+++ archiver.py
@@ -2,9 +2,9 @@ class Archiver:
 	def __init__(token):
 		self.token = token
 		
-	def group_list():
+	def group_list(self):
 		pass #return list of strings of group names
 		
-	def archive_group(groupNo):
+	def archive_group(self, groupNo):
 		pass #create a folder filled with the contents of the group messages
 		
